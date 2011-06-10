b6a110f9e573d3be0c84bc2d4b9cc56bf256f749
Paŭlo Ebermann <paul@fencing-game.de>
2011-06-10 23:24:10 +0200

    ZOutputStream: documented.

--- src/main/java/com/jcraft/jzlib/ZOutputStream.java
+++ src/main/java/com/jcraft/jzlib/ZOutputStream.java
@@ -31,7 +31,9 @@ package com.jcraft.jzlib;
 import java.io.*;
 
 /**
- * ZOutputStream
+ * An output stream wrapper around a {@link ZStream}.
+ * This can work either as a deflating or inflating stream,
+ * depending on the constructor being used.
  *
  * @deprecated  use DeflaterOutputStream or InflaterInputStream
  */
@@ -43,12 +45,21 @@ public class ZOutputStream extends FilterOutputStream {
   protected byte[] buf=new byte[bufsize];
   protected boolean compress;
 
+  /**
+   * The underlying output stream. All compressed (or decompressed)
+   * data will be written to this.
+   */
   protected OutputStream out;
   private boolean end=false;
 
   private DeflaterOutputStream dos;
   private Inflater inflater;
 
+  /**
+   * Creates a new decompressing (inflating) ZOutputStream.
+   * The stream expects the written data to be in zlib format,
+   * and writes the decompressed data to the argument stream.
+   */
   public ZOutputStream(OutputStream out) throws IOException {
     super(out);
     this.out=out;
@@ -57,10 +68,38 @@ public class ZOutputStream extends FilterOutputStream {
     compress=false;
   }
 
+  /**
+   * Creates a new compressing (deflating) ZOutputStream.
+   * The stream will compress any written data, and write
+   * it to the argument stream in zlib format.
+   * @param out the stream that will receive the compressed
+   *   data.
+   * @param level the deflation level. This should be
+   *   {@link JZlib#Z_NO_COMPRESSION Z_NO_COMPRESSION},
+   *   {@link JZlib#Z_DEFAULT_COMPRESSION Z_DEFAULT_COMPRESSION} or a
+   *   value between {@link JZlib#Z_BEST_SPEED Z_BEST_SPEED} (1) and
+   *   {@link JZlib#Z_BEST_COMPRESSION Z_BEST_COMPRESSION} (9) (both inclusive).
+   */
   public ZOutputStream(OutputStream out, int level) throws IOException {
     this(out, level, false);
   }
 
+  /**
+   * Creates a new compressing (deflating) ZOutputStream.
+   * The stream will compress any written data, and write
+   * it to the argument stream in either zlib or plain
+   * deflate format.
+   * @param out the stream that will receive the compressed
+   *   data.
+   * @param level the deflation level. This should be
+   *   {@link JZlib#Z_NO_COMPRESSION Z_NO_COMPRESSION},
+   *   {@link JZlib#Z_DEFAULT_COMPRESSION Z_DEFAULT_COMPRESSION} or a
+   *   value between {@link JZlib#Z_BEST_SPEED Z_BEST_SPEED} (1) and
+   *   {@link JZlib#Z_BEST_COMPRESSION Z_BEST_COMPRESSION} (9) (both inclusive).
+   * @param nowrap if {@code true}, the stream uses the plain deflate
+   *   format. If {@code false}, the stream uses the {@code zlib} format
+   *   (which includes a header and checksum).
+   */
   public ZOutputStream(OutputStream out, int level, boolean nowrap) throws IOException {
     super(out);
     this.out=out;
@@ -69,12 +108,28 @@ public class ZOutputStream extends FilterOutputStream {
     compress=true;
   }
 
+  /**
+   * A one-byte buffer, used by {@link #write(int)}.
+   */
   private byte[] buf1 = new byte[1];
+
+  /**
+   * Writes a single byte. This is a shortcut to calling
+   * {@link #write(byte[], int, int) write(new byte[]{b},0,1)}
+   * (but reusing the array for multiple calls).
+   * @param b the byte to write.
+   */
   public void write(int b) throws IOException {
     buf1[0]=(byte)b;
     write(buf1, 0, 1);
   }
 
+  /**
+   * Writes a sequence of bytes, compressing or decompressing it.
+   * @param b the array holding the data
+   * @param off the offset in {@code b} where the data starts.
+   * @param len the length of the data.
+   */
   public void write(byte b[], int off, int len) throws IOException {
     if(len==0) return;
     if(compress){
@@ -97,14 +152,27 @@ public class ZOutputStream extends FilterOutputStream {
     }
   }
 
+  /**
+   * Returns the current flush mode, which will be used for every
+   * {@link #write}.
+   */
   public int getFlushMode() {
     return flush;
   }
 
+  /**
+   * Sets the flush mode. This will be used for every {@link #write}.
+   */
   public void setFlushMode(int flush) {
     this.flush=flush;
   }
 
+
+  /**
+   * Finishes the compressing/decompressing, without closing the
+   * underlying stream. This will flush the buffer, end the block
+   * and (if not in plain deflate mode) add or compare the checksum.
+   */
   public void finish() throws IOException {
     int err;
     if(compress){
@@ -120,6 +188,12 @@ public class ZOutputStream extends FilterOutputStream {
     }
     flush();
   }
+
+  /**
+   * Cleans up the deflater/inflater state.
+   *
+   * This will be automatically called by {@link #close}.
+   */
   public synchronized void end() {
     if(end) return;
     if(compress){
@@ -130,6 +204,11 @@ public class ZOutputStream extends FilterOutputStream {
     }
     end=true;
   }
+
+  /**
+   * Closes the stream. This will flush out anything written so far,
+   * release any resources, and close the underlying stream, too.
+   */
   public void close() throws IOException {
     try{
       try{finish();}
@@ -152,6 +231,9 @@ public class ZOutputStream extends FilterOutputStream {
     else return inflater.total_out;
   }
 
+  /**
+   * flushes the underlying stream.
+   */
   public void flush() throws IOException {
     out.flush();
   }
