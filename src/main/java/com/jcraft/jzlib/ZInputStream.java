fc8449afd6cc37e5e345bfef22f64703e6e93cdf
Paŭlo Ebermann <paul@fencing-game.de>
2011-06-12 03:17:00 +0200

    ZInputStream: documentation.

--- src/main/java/com/jcraft/jzlib/ZInputStream.java
+++ src/main/java/com/jcraft/jzlib/ZInputStream.java
@@ -31,7 +31,9 @@ package com.jcraft.jzlib;
 import java.io.*;
 
 /**
- * ZInputStream
+ * An input stream wrapper around a {@link ZStream}.
+ * This can work either as a deflating or inflating stream,
+ * depending on the constructor being used.
  *
  * @deprecated  use DeflaterOutputStream or InflaterInputStream
  */
@@ -39,21 +41,51 @@ import java.io.*;
 public class ZInputStream extends FilterInputStream {
 
   protected int flush=JZlib.Z_NO_FLUSH;
+  /**
+   * Wether we are inflating (false) or deflating (true).
+   */
   protected boolean compress;
+  /**
+   * The source input stream. The data will be read from this before
+   * being compressed or decompressed.
+   */
   protected InputStream in=null;
 
   protected Deflater deflater;
   protected InflaterInputStream iis;
 
+  /**
+   * Creates a new decompressing (inflating) ZInputStream
+   * reading zlib formatted data.
+   * @param in the base stream, which should contain data in
+   *   zlib format.
+   */
   public ZInputStream(InputStream in) throws IOException {
     this(in, false);
   }
+  /**
+   * Creates a new decompressing (inflating) ZInputStream,
+   * reading either zlib or plain deflate data.
+   * @param in the base stream, which should contain data
+   *   in the right format.
+   * @param nowrap if true, the input is plain deflate data.
+   *   If false, it is in zlib format (i.e. with a small header
+   *   and a checksum).
+   */
   public ZInputStream(InputStream in, boolean nowrap) throws IOException {
     super(in);
     iis = new InflaterInputStream(in);
     compress=false;
   }
 
+  /**
+   * Creates a compressing (deflating) ZInputStream,
+   * producing zlib format data.
+   * The stream reads uncompressed data from the base
+   * stream, and produces compressed data in zlib format.
+   * @param in the base stream from which to read uncompressed data.
+   * @param level the compression level which will be used.
+   */
   public ZInputStream(InputStream in, int level) throws IOException {
     super(in);
     this.in=in;
@@ -62,14 +94,37 @@ public class ZInputStream extends FilterInputStream {
     compress=true;
   }
 
+   /**
+    * A one-byte buffer, used by {@link #read()}.
+    */
   private byte[] buf1 = new byte[1];
+
+  /**
+   * Reads one byte of data.
+   * @return the read byte, or -1 on end of input.
+   */
   public int read() throws IOException {
     if(read(buf1, 0, 1)==-1) return -1;
     return(buf1[0]&0xFF);
   }
 
+  /**
+    * The internal buffer used for reading the
+    * original stream and passing input to the
+    * deflater.
+    */
   private byte[] buf = new byte[512];
 
+  /**
+   * reads some data from the stream.
+   * This will compress or decompress data from the
+   * underlying stream.
+   * @param b the buffer in which to put the data.
+   * @param off the offset in b on which we should
+   *     put the data.
+   * @param len how much data to read maximally.
+   * @return the amount of data actually read.
+   */
   public int read(byte[] b, int off, int len) throws IOException {
     if(compress){
       deflater.setOutput(b, off, len);
@@ -93,6 +148,13 @@ public class ZInputStream extends FilterInputStream {
     }
   }
 
+  /**
+   * skips some amount of (compressed or decompressed) input.
+   *
+   * In this implementation, we will simply read some data and
+   * discard it. We will skip maximally 512 bytes on each call.
+   * @return the number of bytes actually skipped.
+   */
   public long skip(long n) throws IOException {
     int len=512;
     if(n<len)
@@ -101,10 +163,18 @@ public class ZInputStream extends FilterInputStream {
     return((long)read(tmp));
   }
 
+  /**
+   * Returns the current flush mode used for each compressing/decompressing
+   * call. Normally this should be {@link JZlib#Z_NO_FLUSH Z_NO_FLUSH}.
+   */
   public int getFlushMode() {
     return flush;
   }
 
+  /**
+   * Sets the current flush mode to be used for each compressing/decompressing
+   * call. Normally this should be {@link JZlib#Z_NO_FLUSH Z_NO_FLUSH}.
+   */
   public void setFlushMode(int flush) {
     this.flush=flush;
   }
@@ -119,6 +189,10 @@ public class ZInputStream extends FilterInputStream {
     else return iis.getTotalOut();
   }
 
+  /**
+   * Closes this stream.
+   * This closes the underlying stream, too.
+   */
   public void close() throws IOException{
     if(compress) deflater.end();
     else iis.close();
