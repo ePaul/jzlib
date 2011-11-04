5a4f55124a264d3f1ab0c5a5279510507b3f4e7f
Paŭlo Ebermann <paul@fencing-game.de>
2011-11-05 00:30:41 +0100

    GZIPInputStream: documentation

--- src/main/java/com/jcraft/jzlib/GZIPInputStream.java
+++ src/main/java/com/jcraft/jzlib/GZIPInputStream.java
@@ -30,12 +30,35 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 package com.jcraft.jzlib;
 import java.io.*;
 
+/**
+ * An inflating stream wrapper around a compressed stream in
+ * gzip format.
+ * In addition to its superclass' methods, it has some methods
+ * to access the attributed which might have been encoded in the
+ * gzip header.
+ * @see <a href="http://tools.ietf.org/html/rfc1952">RFC 1952: GZIP file
+ *    format specification version 4.3</a>
+ */
 public class GZIPInputStream extends InflaterInputStream {
 
+  /**
+   * Creates a GZIPInputStream using the default buffer size.
+   * @param in the source stream from which to read the gzip data.
+   *   It will be closed when this stream is closed.
+   */
   public GZIPInputStream(InputStream in) throws IOException {
     this(in, DEFAULT_BUFSIZE, true);
   }
 
+  /**
+   * Creates a GZIPInputStream.
+   * @param in the source stream from which to read the gzip data.
+   * @param size the size of the input buffer used for reading from the
+   *   source stream.
+   * @param close_in if {@code true}, the source input stream will closed
+   *  on closing this stream. If {@code false}, it will left open, allowing
+   *  more data to be read from it directly.
+   */
   public GZIPInputStream(InputStream in,
                          int size,
                          boolean close_in) throws IOException {
@@ -43,6 +66,17 @@ public class GZIPInputStream extends InflaterInputStream {
     myinflater = true;
   }
 
+  /**
+   * Creates a GZIPInputStream.
+   * @param in the source stream from which to read the gzip data.
+   * @param inflater the inflater used to decompress the data. This deflater
+   *   should know how to read the gzip header data.
+   * @param size the size of the input buffer used for reading from the
+   *   source stream.
+   * @param close_in if {@code true}, the source input stream will closed
+   *  on closing this stream. If {@code false}, it will left open, allowing
+   *  more data to be read from it directly.
+   */
   public GZIPInputStream(InputStream in, 
                          Inflater inflater,
                          int size,
@@ -50,28 +84,84 @@ public class GZIPInputStream extends InflaterInputStream {
     super(in, inflater, size, close_in);
   }
 
+  /**
+   * Gets the modification time from the gzip file header.
+   * @return the recorded modification time as seconds since the
+   *    Unix Epoch (1970-01-01 00:00 GMT). If 0, no time stamp is
+   *   available.
+   * @see <a href="http://tools.ietf.org/html/rfc1952#page-7">RFC 1952,
+   *      section 2.3.1</a>
+   */
   public long getModifiedtime() {
     return inflater.istate.getGZIPHeader().getModifiedTime();
   }
 
+  /**
+   * Gets the recorded operation system flag.
+   * @return one of these values:<pre>
+   *      0 - FAT filesystem (MS-DOS, OS/2, NT/Win32)
+   *      1 - Amiga
+   *      2 - VMS (or OpenVMS)
+   *      3 - Unix
+   *      4 - VM/CMS
+   *      5 - Atari TOS
+   *      6 - HPFS filesystem (OS/2, NT)
+   *      7 - Macintosh
+   *      8 - Z-System
+   *      9 - CP/M
+   *     10 - TOPS-20
+   *     11 - NTFS filesystem (NT)
+   *     12 - QDOS
+   *     13 - Acorn RISCOS
+   *    255 - unknown</pre>
+   * @see <a href="http://tools.ietf.org/html/rfc1952#page-7">RFC 1952,
+   *      section 2.3.1</a>
+   */
   public int getOS() {
     return inflater.istate.getGZIPHeader().getOS();
   }
 
+  /**
+   * Gets the recorded file name.
+   * @return {@code ""} (the empty string) if there is no recorded file name,
+   *   otherwise the file name as recorded in the gzip header.
+   * @see <a href="http://tools.ietf.org/html/rfc1952#page-7">RFC 1952,
+   *      section 2.3.1</a>
+   */
   public String getName() {
     return inflater.istate.getGZIPHeader().getName();
   }
 
+  
+  /**
+   * Gets the recorded file comment.
+   * @return {@code ""} (the empty string) if there is no recorded file comment,
+   *   otherwise the file comment as recorded in the gzip header.
+   * @see <a href="http://tools.ietf.org/html/rfc1952#page-7">RFC 1952,
+   *      section 2.3.1</a>
+   */
   public String getComment() {
     return inflater.istate.getGZIPHeader().getComment();
   }
 
+  /**
+   * Gets the checksum value.
+   * This method can only be called after the end of a gzip-compressed
+   * data block (as the checksum is stored at the end).
+   * @return the CRC-32 check sum of the uncompressed data.
+   * @see <a href="http://tools.ietf.org/html/rfc1952#page-8">RFC 1952,
+   *      section 2.3.1</a>
+   */
   public long getCRC() throws GZIPException {
     if(inflater.istate.mode != 12 /*DONE*/)
       throw new GZIPException("checksum is not calculated yet.");
     return inflater.istate.getGZIPHeader().getCRC();
   }
 
+
+  /**
+   * Reads a gzip header from the source stream.
+   */
   public void readHeader() throws IOException {
 
     byte[] empty = "".getBytes();
@@ -124,6 +214,9 @@ public class GZIPInputStream extends InflaterInputStream {
     while(inflater.istate.inParsingHeader());
   }
 
+  /**
+   * fills a buffer with data from the source stream.
+   */
   private int fill(byte[] buf) {
     int len = buf.length;
     int n = 0;
